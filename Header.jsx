e6b90cad550d19ee534e9767b6ce843c797ae088
Palak Tyagi <eduvert77@gmail.com>
2026-04-05 19:17:20 +0530

    Add Header.jsx component

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+// Header Component
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Welcome to our Cafe!</h1>
+        </header>
+    );
+};
+
+export default Header;
